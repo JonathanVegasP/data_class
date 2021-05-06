d30c6da4bd38b9c887deb9ab85057728d220b6c0
Jonathan Vegas Peixoto <jopxoto12@gmail.com>
2021-05-06 15:24:05 -0300

    initial commit

---
+++ lib/data_class.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library data_class;
+
+export 'src/data_class.dart';
+
+// TODO: Export any libraries intended for clients of this package.
---
+++ lib/src/data_class.dart
@@ -0,0 +1,5 @@
+class DataClass {
+  const DataClass();
+}
+
+const data = DataClass();
